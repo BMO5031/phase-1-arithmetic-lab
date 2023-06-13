99dda3c6e532e3f256ad6c604cafdc8388fd8118
USER <+>
2023-06-13 15:59:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1 =2;
+const num2 =31;
+
+const multiply= num1*num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() *100) + 1;
+console.log(random);
+
+const num3 = 40;
+const num4 =6
+const mod = num3 % num4
+
+const set = [0, 5, 10, 15, 20]
+const max = Math.max(...set);
+console.log(max);
+
+
